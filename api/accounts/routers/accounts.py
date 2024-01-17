1f1eadd583520b505d428507e6e2957a3e2edbad
curioquest <agifford101.dev@gmail.com>
2024-01-16 22:11:22 -0800

    get_token changed back to original

--- api/accounts/routers/accounts.py
+++ api/accounts/routers/accounts.py
@@ -100,8 +100,7 @@ async def update_account_info(
 async def get_token(
     request: Request,
     account: AccountOut = Depends(authenticator.try_get_current_account_data)
-):
-# -> AccountToken | None:
+) -> AccountToken | None:
     if account and authenticator.cookie_name in request.cookies:
         return {
             "access_token": request.cookies[authenticator.cookie_name],
