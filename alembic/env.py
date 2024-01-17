31c569f023e3eefc956c0d6f8a2bbb9c2870fa61
curioquest <agifford101.dev@gmail.com>
2024-01-16 19:29:27 -0800

    alembic files successfully created no errors

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,37 @@
+# from dotenv import load_dotenv
 from logging.config import fileConfig
-
+# import os
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+import sys
+sys.path = ['', '..'] + sys.path[1:]
+import configobj
+
+from base import Base
+
+
+
+# load_dotenv('./.env')
+
+# Create a combined metadata object from multiple model files
+
+target_metadata = Base.metadata
+
+# Merge metadata from different files into a single target_metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config2 = configobj.ConfigObj('.env')
+alembic_db_url = config2['ALEMBIC_DB_URL']
+
+
+# alembic_db_url = os.environ.get('ALEMBIC_DB_URL')
+
+# Set the database URL in Alembic's context
+# context.config.set_main_option("sqlalchemy.url", alembic_db_url)
+config.set_main_option('sqlalchemy.url', str(alembic_db_url))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +42,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- alembic/versions/19555e650efb_new_tables_for_managed_database.py
+++ alembic/versions/19555e650efb_new_tables_for_managed_database.py
@@ -1,8 +1,8 @@
 """new tables for managed database
 
-Revision ID: 80c29e3d0c53
+Revision ID: 19555e650efb
 Revises: 
-Create Date: 2024-01-16 19:18:34.245532
+Create Date: 2024-01-16 19:28:46.853845
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '80c29e3d0c53'
+revision: str = '19555e650efb'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
